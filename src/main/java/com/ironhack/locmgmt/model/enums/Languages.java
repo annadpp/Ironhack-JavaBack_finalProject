223116865ecb18f2d17c0ab4766cb1192fb1505a
annadpp <anna.depablo.puig@gmail.com>
2024-04-23 16:40:31 +0200

    Add enums

---
+++ src/main/java/com/ironhack/locmgmt/model/enums/Languages.java
@@ -0,0 +1,15 @@
+package com.ironhack.locmgmt.model.enums;
+
+public enum Languages {
+    SPANISH,
+    ENGLISH,
+    CATALAN,
+    GERMAN,
+    FRENCH,
+    SWEDISH,
+    FINNISH,
+    ITALIAN,
+    JAPANESE,
+    CHINESE,
+    PORTUGUESE
+}
