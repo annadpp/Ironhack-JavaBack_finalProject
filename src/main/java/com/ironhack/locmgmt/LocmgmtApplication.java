9029c1b620195686137277d19bd66b3013750e54
annadpp <anna.depablo.puig@gmail.com>
2024-04-23 13:49:49 +0200

    Base project

---
+++ src/main/java/com/ironhack/locmgmt/LocmgmtApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.locmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocmgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocmgmtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ironhack/locmgmt/LocmgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.locmgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocmgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
