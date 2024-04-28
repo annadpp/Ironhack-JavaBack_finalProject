250401516c101034197f26c3562082280a118ff6
annadpp <anna.depablo.puig@gmail.com>
2024-04-28 20:39:10 +0200

    Add Linguist exceptions

--- src/main/java/com/ironhack/locmgmt/controller/LinguistController.java
+++ src/main/java/com/ironhack/locmgmt/controller/LinguistController.java
@@ -2,9 +2,9 @@ package com.ironhack.locmgmt.controller;
 
 import com.ironhack.locmgmt.model.users.Linguist;
 import com.ironhack.locmgmt.service.LinguistService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
--- src/main/java/com/ironhack/locmgmt/service/LinguistService.java
+++ src/main/java/com/ironhack/locmgmt/service/LinguistService.java
@@ -1,8 +1,12 @@
 package com.ironhack.locmgmt.service;
 
+import com.ironhack.locmgmt.exception.EmptyListException;
 import com.ironhack.locmgmt.model.users.Linguist;
 import com.ironhack.locmgmt.repository.LinguistRepository;
+
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.*;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,25 +17,61 @@ public class LinguistService {
     private LinguistRepository linguistRepository;
 
     public List<Linguist> getAllLinguists() {
-        return linguistRepository.findAll();
+        try {
+            List<Linguist> linguists = linguistRepository.findAll();;
+            if (linguists.isEmpty()) {
+                throw new EmptyListException("No linguists were found");
+            }
+            return linguists;
+        } catch (DataAccessException e) {
+            throw new DataRetrievalFailureException("Error while retrieving all rates", e);
+        }
     }
 
     public Linguist getLinguistById(Long id) {
-        return linguistRepository.findById(id).orElse(null);
+        return linguistRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Linguist not found with id: " + id));
+
     }
 
     public Linguist createLinguist(Linguist linguist) {
-        return linguistRepository.save(linguist);
+        try {
+            return linguistRepository.save(linguist);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityViolationException("Error while creating the linguist", e);
+        }
     }
 
-    public Linguist updateLinguist(Long linguistId, Linguist linguistDetails) {
-        Linguist existingLinguist = linguistRepository.findById(linguistId)
-                .orElseThrow(() -> new RuntimeException("Linguist not found with id: " + linguistId));
+    public Linguist updateLinguist(Long id, Linguist linguistDetails) {
+        Linguist existingLinguist = linguistRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Linguist not found with id: " + id));
 
-        existingLinguist.setLanguages(linguistDetails.getLanguages());
-        existingLinguist.setProjectTypes(linguistDetails.getProjectTypes());
-        existingLinguist.setDtpTechnologies(linguistDetails.getDtpTechnologies());
-        existingLinguist.setLinguisticTechnologies(linguistDetails.getLinguisticTechnologies());
+        // Update the inherent Admin fields passed
+        if (linguistDetails.getLanguages() != null) {
+            existingLinguist.setLanguages(linguistDetails.getLanguages());
+        }
+
+        if (linguistDetails.getProjectTypes() != null) {
+            existingLinguist.setProjectTypes(linguistDetails.getProjectTypes());
+        }
+
+        if (linguistDetails.getDtpTechnologies() != null) {
+            existingLinguist.setDtpTechnologies(linguistDetails.getDtpTechnologies());
+        }
+
+        if (linguistDetails.getLinguisticTechnologies() != null) {
+            existingLinguist.setLinguisticTechnologies(linguistDetails.getLinguisticTechnologies());
+        }
+
+        // Update the fields inherited from the User class
+        if (linguistDetails.getUsername() != null) {
+            existingLinguist.setUsername(linguistDetails.getUsername());
+        }
+        if (linguistDetails.getName() != null) {
+            existingLinguist.setName(linguistDetails.getName());
+        }
+        if (linguistDetails.getEmail() != null) {
+            existingLinguist.setEmail(linguistDetails.getEmail());
+        }
 /*
         existingLinguist.setRates(linguistDetails.getRates());
 */
@@ -39,7 +79,13 @@ public class LinguistService {
         return linguistRepository.save(existingLinguist);
     }
 
-    public void deleteLinguist(Long LinguistId) {
-        linguistRepository.deleteById(LinguistId);
+    public void deleteLinguist(Long id) {
+        try {
+            linguistRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new EntityNotFoundException("Linguist not found with id: " + id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityViolationException("Linguist deleting rate with id: " + id);
+        }
     }
 }
