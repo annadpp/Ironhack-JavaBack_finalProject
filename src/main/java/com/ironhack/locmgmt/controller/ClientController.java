0e354630ef0be5360b4231cf0eef6e3543c3a3f0
annadpp <anna.depablo.puig@gmail.com>
2024-04-28 10:35:06 +0200

    Update endpoints to ResponseStatus

--- src/main/java/com/ironhack/locmgmt/controller/ClientController.java
+++ src/main/java/com/ironhack/locmgmt/controller/ClientController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -16,46 +17,55 @@ public class ClientController {
     private ClientService clientService;
 
     @GetMapping("/get")
-    public ResponseEntity<List<Client>> getAllClients() {
-        List<Client> clients = clientService.getAllClients();
-        return new ResponseEntity<>(clients, HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    public List<Client> getAllClients() {
+        return clientService.getAllClients();
     }
 
     @GetMapping("/get/{id}")
-    public ResponseEntity<Client> getClientById(@PathVariable Long id) {
+    @ResponseStatus(HttpStatus.OK)
+    public Client getClientById(@PathVariable Long id) {
         Client client = clientService.getClientById(id);
-        if (client != null) {
-            return new ResponseEntity<>(client, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        if (client == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Client not found");
         }
+        return client;
     }
 
     @PostMapping("/save")
-    public ResponseEntity<Client> createClient(@RequestBody Client client) {
-        Client createdClient = clientService.createClient(client);
-        return new ResponseEntity<>(createdClient, HttpStatus.CREATED);
+    @ResponseStatus(HttpStatus.CREATED)
+    public Client createClient(@RequestBody Client client) {
+        return clientService.createClient(client);
     }
 
     @PutMapping("/update/{id}")
-    public ResponseEntity<Client> updateClient(@PathVariable Long id, @RequestBody Client client) {
-        Client updatedClient = clientService.updateClient(id, client);
-        return new ResponseEntity<>(updatedClient, HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    public Client updateClient(@PathVariable Long id, @RequestBody Client client) {
+        return clientService.updateClient(id, client);
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<?> deleteClient(@PathVariable Long id) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteClient(@PathVariable Long id) {
         clientService.deleteClient(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @GetMapping("/get/client")
+    @ResponseStatus(HttpStatus.OK)
     public List<Client> getClientByName(@RequestParam String name) {
-        return clientService.getClientByName(name);
-    }
+        List<Client> clients = clientService.getClientByName(name);
+        if (clients.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Client not found");
+        }
+        return clients;    }
 
     @GetMapping("/get/email")
+    @ResponseStatus(HttpStatus.OK)
     public List<String> getClientEmailByName(@RequestParam String name) {
-        return clientService.getClientEmailByName(name);
+        List<String> emails = clientService.getClientEmailByName(name);
+        if (emails.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Email not found");
+        }
+        return emails;
     }
 }
